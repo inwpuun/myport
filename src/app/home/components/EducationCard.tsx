23395e61d22d5ea58c8c3cd38ef140f10919e0a2
inwpuun <112232858+inwpuun@users.noreply.github.com>
2024-11-05 00:33:19 +0700

    feat: responsive

--- src/app/home/components/EducationCard.tsx
+++ src/app/home/components/EducationCard.tsx
@@ -11,8 +11,8 @@ type EducationCardProps = {
 export default function EducationCard(props: EducationCardProps) {
   const { title, imagesrc, period, description } = props;
   return (
-    <div className="flex gap-5 items-center min-w-[384px] w-full hover:scale-105 transition-transform duration-300">
-    <Image
+    <div className="flex gap-5 items-center w-full hover:scale-105 transition-transform duration-300">
+      <Image
         src={imagesrc}
         alt={`${title} education`}
         width={80}
@@ -20,11 +20,11 @@ export default function EducationCard(props: EducationCardProps) {
         className="rounded-full border-4 border-blue-700 transform hover:scale-110 transition-transform duration-300 ease-in-out"
       />
       <div className="flex flex-col gap-2 w-full">
-        <div className="flex justify-between">
-          <p className="text-lg font-bold transition-colors duration-300 hover:text-blue-700">{title}</p>
-          <p className="text-md text-gray-400 transition-colors duration-300 hover:text-gray-600">{period}</p>
+        <div className="flex justify-between max-sm:flex-col lg:flex-col">
+          <p className="text-md font-bold transition-colors duration-300 hover:text-blue-700">{title}</p>
+          <p className="text-sm text-gray-400 transition-colors duration-300 hover:text-gray-600">{period}</p>
         </div>
-        <p className="text-md text-gray-400 transition-colors duration-300 hover:text-gray-600">{description}</p>
+        <p className="text-sm text-gray-400 transition-colors duration-300 hover:text-gray-600">{description}</p>
       </div>
     </div>
   );
