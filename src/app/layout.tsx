dab5ea9497018bd72dff675c1b308fa027c54722
inwpuun <112232858+inwpuun@users.noreply.github.com>
2024-12-06 18:08:43 +0700

    feat: add SpeedInsights component to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
       <body>
         <BackgroundProvider>{children}</BackgroundProvider>
         <Toast theme="dark"  />
+        <SpeedInsights />
       </body>
     </html>
   );
